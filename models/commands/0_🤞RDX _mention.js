969ad72fac10983b7d15aaee21e9a87d19466d89
HANIA-AMIR <haniaamirxyz123@gmail.com>
2024-12-31 18:48:27 +0500

    Add files via upload

---
+++ models/commands/0_🤞RDX _mention.js
@@ -0,0 +1,22 @@
+module.exports.config = {
+  name: "goiadmin",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "RDX_ZAIN",
+  description: "Bot will rep ng tag admin or rep ng tagbot ",
+  commandCategory: "Other",
+  usages: "",
+  cooldowns: 1
+};
+module.exports.handleEvent = function({ api, event }) {
+  if (event.senderID !== "100024385579728") {
+    var aid = ["100024385579728"];
+    for (const id of aid) {
+    if ( Object.keys(event.mentions) == id) {
+      var msg = ["Wo Busy H mujhe Bolo Kya Bolna H?", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy hoga", "Ahmad Toh Chala gaya"];
+      return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+    }
+    }}
+};
+module.exports.run = async function({}) {
+        }
